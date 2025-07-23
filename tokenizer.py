cc3816a1a24373f353b0f5198d1174ff7ce2f3a7
boochanggyu <qnckdrb11@naver.com>
2025-07-23 21:44:55 +0900

    update

--- tokenizer.py
+++ tokenizer.py
@@ -1,4 +1,5 @@
 from transformers import AutoTokenizer
 
 def tokenize(example):
+    tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.3")
     return tokenizer(example["instruction"] + "\n" + example["output"], truncation=True, padding="max_length", max_length=512)
