f7a07236a56445edb16b76ccc0757176ce079c7f
부창규 <79037397+genji970@users.noreply.github.com>
2025-07-31 05:03:50 +0900

    Delete data directory

--- data/data_score_func.py
+++
@@ -1,9 +0,0 @@
-from sentence_transformers import SentenceTransformer, util
-
-model = SentenceTransformer("all-MiniLM-L6-v2")
-
-def compute_reward(instruction, output):
-    emb_q = model.encode(instruction, convert_to_tensor=True)
-    emb_a = model.encode(output, convert_to_tensor=True)
-    sim = util.cos_sim(emb_q, emb_a).item()
-    return round(min(max(sim, 0.0), 1.0), 3)
